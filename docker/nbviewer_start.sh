b8c7b1705e4a58a796a49391ca3a54897434faf9
Pan Guolin <Guolin.Pan@dell.com>
2018-07-26 13:44:47 +0800

    nbviewer_start.sh: add argument '--no-cache'

--- docker/nbviewer_start.sh
+++ docker/nbviewer_start.sh
@@ -6,7 +6,7 @@ host_port=10002
 
 # start
 mkdir -p $host_notebook_dir
-sudo docker run --rm -it -d  --name $instance_name -v $host_notebook_dir:/tmp -p $host_port:8080 jupyter/nbviewer python3 -m nbviewer --localfiles=/tmp --port=8080
+sudo docker run --rm -it -d  --name $instance_name -v $host_notebook_dir:/tmp -p $host_port:8080 jupyter/nbviewer python3 -m nbviewer --localfiles=/tmp --port=8080 --no-cache
 
 ret=$?
 if [ $ret -eq 0 ]; then
